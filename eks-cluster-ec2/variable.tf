4c010dd8235ba96d6a81b1402bc13d6ff3cabe98
favour <favourt80@yahoo.ca>
2023-09-27 21:13:42 -0600

    new added

--- eks-cluster-ec2/variable.tf
+++ eks-cluster-ec2/variable.tf
@@ -3,11 +3,11 @@ variable "cluster_name" {
 }
 
 variable "cluster_version" {
-  default = "1.25"
+  default = "1.27"
 }
 
 variable "region" {
-  default = "us-east-1"
+  default = "ca-central-1"
 }
 
 variable "ingress_ports" {
